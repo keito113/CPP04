9f2de018f7d7b3892a13fde665104ee1d7f4bc76
Keito Abe <keitabe@c1r4s7.42tokyo.jp>
2026-05-20 18:16:17 +0900

    fix/ex00/Makefile, ex01/create

---
+++ ex01/Cat.cpp
@@ -0,0 +1,25 @@
+#include "Cat.hpp"
+#include <iostream>
+
+Cat::Cat(void) : Animal("Cat") {
+	std::cout << "Cat default constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat& other) : Animal(other) {
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat& Cat::operator=(const Cat& other) {
+	std::cout << "Cat copy assignment operator called" << std::endl;
+	if (this != &other)
+		Animal::operator=(other);
+	return (*this);
+}
+
+Cat::~Cat(void) {
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound(void) const {
+	std::cout << "Cat: * meow meow *" << std::endl;
+}
